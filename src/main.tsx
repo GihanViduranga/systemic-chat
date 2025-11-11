24e6931b1fe64895d08870ef6b046f45d1ad018e
gihanviduranga <gihanvidu123@gmai.com>
2025-11-12 01:03:55 +0530

    Added logo as the site favicon

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import systemicLogo from './assets/systemicLogo.png'
+
+const favicon = document.querySelector<HTMLLinkElement>("link[rel*='icon']")
+if (favicon) {
+  favicon.href = systemicLogo
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
